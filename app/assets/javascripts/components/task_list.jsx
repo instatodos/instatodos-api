c88bc3d43dbb8f4a8375b2b8c860f6ed4d7342c2
Sergio D. Márquez <marquez.sergio.d@gmail.com>
2016-02-25 22:26:02 -0300

    "Fetch" data in js instead of erb

--- app/assets/javascripts/components/task_list.jsx
+++ app/assets/javascripts/components/task_list.jsx
@@ -1,10 +1,24 @@
 class TaskList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { tasks: [] };
+  }
+
+  componentDidMount () {
+    this.fetchTasks()
+    setInterval(this.fetchTasks(), 2000);
+  }
+
+  fetchTasks () {
+    let data = {tasks: [
+      { id: 1, title: 'hola', description: 'desc', completed: true },
+      { id: 2, title: 'hola 2', description: 'desc 2', completed: false }
+    ]}
+    this.setState(data)
+  }
+
   render () {
     let createItem = (task) => <Task key={task.id} title={task.title} description={task.description} completed={task.completed} />
-    return <ul> {this.props.tasks.map(createItem)} </ul>;
+    return <ul> {this.state.tasks.map(createItem)} </ul>
   }
 }
-
-TaskList.propTypes = {
-  tasks: React.PropTypes.array
-};
