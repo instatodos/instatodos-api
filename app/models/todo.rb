b0c56581decbd4c0b47850373f5d08c800e816ef
Sergio Marquez <marquez.sergio.d@gmail.com>
2016-04-07 14:43:42 -0300

    Add title validation (backend)

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -3,4 +3,6 @@ class Todo < ApplicationRecord
   friendly_id :title
 
   has_many :tasks
+
+  validates :title, format: { with: /\w+-\w+-\d{3}/, message: 'not allowed' }
 end
--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -2,4 +2,7 @@ require 'rails_helper'
 
 describe Todo do
   it { is_expected.to have_many(:tasks) }
+  it { expect(subject).not_to allow_value("hola").for(:title) }
+  it { expect(subject).not_to allow_value("hola-chau").for(:title) }
+  it { expect(subject).to allow_value("hola-chau-123").for(:title) }
 end
