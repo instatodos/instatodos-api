e38481e7d786c584e98c09fb120ab842309e6e56
Sergio D. Márquez <marquez.sergio.d@gmail.com>
2017-03-21 15:14:18 +1300

    Upgrade rails

--- app/channels/todo_channel.rb
+++ app/channels/todo_channel.rb
@@ -1,56 +1,42 @@
 class TodoChannel < ApplicationCable::Channel
   def follow(data)
-    # stop_all_streams
-    # todo_list_id = data["todo_list_id"]
-    @todo_list ||= TodoList.first
-    @todos ||= @todo_list.todos
+    @todo_list  ||= TodoList.first
+    @todos      ||= @todo_list.todos
     @channel_id ||= "todo_channel_#{@todo_list.id}"
     stream_from @channel_id
-    broadcast({ todos: @todos }, :follow)
+    ActionCable.server.broadcast @channel_id, todos: @todos, action: :follow
   end
 
-  # def unfollow
-  #   stop_all_streams
-  # end
-
-  def create_todo(data)
+  def create(data)
     todo = @todos.create(data['todo_params']).to_json
-    broadcast({ todo: todo }, :createTodo)
+    ActionCable.server.broadcast @channel_id, todo: todo, action: :create
   end
 
-  def update_todo(data)
-    # TODO: look for todo in cache
-    # update cache(or @todo variable, assign_attributes), only persist if flag is true
-    # set editing flag to true unless persist
-    # so it basically persists when it's not being edited, or not in focus
-    todo_params = data["todo_params"]
-    persist = data["persist"]
-    todo = find_todo(todo_params["id"]).tap do |t|
-      update_method = persist ? :update_attributes : :assign_attributes
-      t.send update_method, todo_params
-      t.to_json
-    end
-
-    broadcast({ todo: todo }, :updateTodo)
+  def update(data)
+    puts "PERSIST: " + data.to_s
+    persist = data['persist']
+    todo_params = data['todo_params']
+    todo = assign_or_update(todo_params, persist)
+    ActionCable.server.broadcast @channel_id, todo: todo, action: :update
   end
 
-  def delete_todo(data)
-    todo = find_todo(data["id"]).tap do |t|
-      t.destroy!
-      t.to_json
-    end
-
-    broadcast({ todo: todo }, :deleteTodo)
+  def delete(data)
+    todo = find(data['id']).tap { |t| t.destroy!.to_json }
+    ActionCable.server.broadcast @channel_id, todo: todo, action: :delete
   end
 
   private
 
-  def broadcast(message, action)
-    ActionCable.server.broadcast @channel_id, message: message, action: action
+  def find(id)
+    Todo.find id
   end
 
-  def find_todo(id)
-    # @todos.find ?
-    Todo.find(id)
+  def assign_or_update(todo_params, persist)
+    id = todo_params['id']
+    find(id).tap do |t|
+      update_method = persist ? :update_attributes : :assign_attributes
+      t.send update_method, todo_params
+      t.to_json
+    end
   end
 end
